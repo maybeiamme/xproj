7337cb9014a778375daafb3989fdf27168966ced
Jin Hyong Park <jin@propertyguru.com.sg>
2017-07-13 12:38:52 +0800

    Initial commit

---
+++ xproj/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  xproj
+//
+//  Created by Jin Hyong Park on 13/7/17.
+//  Copyright © 2017 PropertyGuru. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
